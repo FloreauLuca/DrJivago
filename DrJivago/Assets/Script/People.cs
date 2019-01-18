ae7f09b11dee5772429207ea2caebb79eb856332
Unknown <lucafloreau@akeonet.com>
2019-01-18 17:34:06 +0100

    manifesant

---
+++ DrJivago/Assets/Script/People.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class People : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        playerTransform = GameManager.Instance.Player.transform;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    [SerializeField] private float peopleSpeed = 1;
+    
+    void Update()
+    {
+        
+        if (playerTransform.position.x < transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z - 20);
+            Debug.Log("right");
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z + 20);
+            Debug.Log("left");
+
+        }
+        
+        rigidbody2D.velocity = (transform.rotation * Vector3.up * 5) - new Vector3(0, (MapManager.Instance.Speed * Time.deltaTime * (5+peopleSpeed)));
+        Debug.Log(transform.rotation * Vector3.up);
+        Debug.Log(new Vector3(0, (MapManager.Instance.Speed * Time.deltaTime * (5 + peopleSpeed))));
+        if (transform.position.y <= -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ DrJivago/Assets/Script/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int life;
+    [SerializeField] private float invicibilityCooldown;
+
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hurt()
+    {
+        life--;
+        if (life > 0)
+        {
+            StartCoroutine(Invicibility());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private IEnumerator Invicibility()
+    {
+        spriteRenderer.color = new Color(1, 1, 1, 0.5f);
+        yield return new WaitForSeconds(invicibilityCooldown);
+        spriteRenderer.color = new Color(1, 1, 1, 1);
+    }
+}
