fde17bc4bc68e8b9f681b39f5ee514ad16593465
Guillaume <j6.guillaume@gmail.com>
2019-01-20 21:16:11 +0100

    added direction buttons

---
+++ DrJivago/Assets/Script/RightButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class RightButton : MonoBehaviour, IPointerDownHandler
+{
+	private PlayerMovement myPlayer;
+
+	void Start()
+	{
+		myPlayer = FindObjectOfType<PlayerMovement>();
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		myPlayer.MoveRight();
+	}
+
+	public void StopRight()
+	{
+		myPlayer.StopMoveRight();
+	}
+}
